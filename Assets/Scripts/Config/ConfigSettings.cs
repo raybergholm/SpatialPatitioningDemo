700562c9250437216414e32f79895f86cd824c39
raybergholm <nalamint@gmail.com>
2016-12-23 15:33:08 +0200

    added references to simulation space (the 2D plane which will be used)

--- Assets/Scripts/Config/ConfigSettings.cs
+++ Assets/Scripts/Config/ConfigSettings.cs
@@ -12,6 +12,12 @@ public static partial class ConfigSettings
     public static string ScenesRootPath { get { return scenesRootPath; } }
     public static string PrefabsRootPath { get { return prefabsRootPath; } }
 
+    public static float DefaultSimulationBoundsHeight { get { return defaultSimulationBoundsHeight; } }
+    public static float DefaultSimulationBoundsWidth { get { return defaultSimulationBoundsWidth; } }
+
     private static string scenesRootPath = "./Scenes/";
     private static string prefabsRootPath = "./Prefabs/";
+
+    private static float defaultSimulationBoundsHeight = 200;
+    private static float defaultSimulationBoundsWidth = 200;
 }
--- Assets/Scripts/SimulationModel.cs
+++ Assets/Scripts/SimulationModel.cs
@@ -4,11 +4,18 @@ namespace SpatialPartitioning
 {
     public class SimulationModel : MonoBehaviour
     {
-        SimulationController controller;
+
+
+        private SimulationController controller;
+
+        [SerializeField]
+        private Bounds plane2D;
 
         private void Awake()
         {
             controller = this.GetComponent<SimulationController>();
+
+            InitModel();
         }
 
         // Use this for initialization
@@ -21,6 +28,16 @@ namespace SpatialPartitioning
         private void Update()
         {
 
+        }
+
+        private void InitModel()
+        {
+            if (plane2D == null)
+            {
+                Rect configPlane = new Rect(new Vector2(0, 0), new Vector2(ConfigSettings.DefaultSimulationBoundsHeight, ConfigSettings.DefaultSimulationBoundsWidth));
+                plane2D = new Bounds();
+            }
+            
         }
     }
 }
