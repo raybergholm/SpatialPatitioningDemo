e28b6c9aecc2672cab2143b7884d79c7fa93873f
raybergholm <nalamint@gmail.com>
2017-01-04 20:44:58 +0200

    added utils

---
+++ Assets/Scripts/SpatialPartitioningUtils.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace SpatialPartitioning
+{
+    public static class SpatialPartitioningUtils
+    {
+        public static Bounds RectToBounds(Rect input)
+        {
+            return new Bounds
+            {
+                center = new Vector3(),
+                extents = new Vector3()
+            };
+        }
+
+
+        public static Rect BoundsToRect(Bounds input)
+        {
+            return new Rect
+            {
+                position = new Vector2(),
+                size = new Vector2()
+            };
+        }
+    }
+}
